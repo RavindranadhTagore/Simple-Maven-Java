bcb1812a8b3267028e31180e06b1033b423c8a41
RavindranadhTagore <137029385+RavindranadhTagore@users.noreply.github.com>
2025-04-18 13:32:38 +0530

    Update HelloWorld.java

--- src/main/java/com/example/HelloWorld.java
+++ src/main/java/com/example/HelloWorld.java
@@ -1,13 +1,5 @@
-
-import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.assertTrue;
-
-public class HelloWorldTest {
-
-    @Test
-    public void testMainMethodRuns() {
-        // Just a dummy test to verify HelloWorld runs
-        HelloWorld.main(new String[]{});
-        assertTrue(true); // if no exception is thrown, we consider it passed
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, Jenkins + Maven!");
     }
 }
